ff0515d3de0f955affe29f18741051aa81aba4a8
Patrick Geiger <patrickjgeiger@gmail.com>
2015-06-25 18:33:49 -0400

    Shields
    Shields added in and fixed the ship not moving.

--- Space SHMUP Prototype/Assets/__Scripts/Hero.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -2,14 +2,39 @@
 using System.Collections;
 
 public class Hero : MonoBehaviour {
+    static public Hero S; //Singleton
 
+    //these fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    //ship status information
+    public float shieldLevel = 1;
+    public bool ______________________;
+
+    void Awake()
+    {
+        S = this; //Set the Singleton
+    }
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    //pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        //change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        //rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 	}
 }
---
+++ Space SHMUP Prototype/Assets/__Scripts/Shield.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+    public float rotationsPerSecond = 0.1f;
+    public bool _____________________________;
+    public int levelShown = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //read the current shield level from the hero singleton
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        //if this is different from levelShown
+        if (levelShown != currLevel)
+        {
+            levelShown = currLevel;
+            Material mat = this.GetComponent<Renderer>().material;
+            //adjust the texture offset to show different shield level
+            mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);
+        }//end if statement
+        //rotate the shield a bit every second
+        float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+	}
+}
