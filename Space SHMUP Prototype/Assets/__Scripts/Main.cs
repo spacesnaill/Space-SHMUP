0b7c5c2ef088e0726d12f9d71f23c36a92392597
Patrick Geiger <patrickjgeiger@gmail.com>
2015-06-25 21:45:35 -0400

    Enemy Spawning
    Enemy Spawning implemented. Currently nothing happens when they impact
    the Hero.

--- Space SHMUP Prototype/Assets/__Scripts/Main.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Main.cs
@@ -1,8 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic; //required to use Lists or Dictionaries
 public class Main : MonoBehaviour {
+    static public Main S; //singleton
 
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f; // the # enemies/second
+    public float enemySpawnPadding = 1.5f; //Padding for position
+
+    public bool _________________;
+
+    public float enemySpawnRate; //delay between enemy spawns
+
+    void Awake()
+    {
+        S = this;
+        //Set utils.camBounds
+        Utils.SetCameraBounds(this.GetComponent<Camera>());
+        //0.5 enemies/second = enemySpawnRate of 2
+        enemySpawnRate = 1f / enemySpawnPerSecond;
+        //Invoke call SpawnEnemy() once after a 2 second delay
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+
+    public void SpawnEnemy()
+    {
+        //pick a random enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate(prefabEnemies[ndx]) as GameObject;
+        //poistion the enemy above the screen with a random x position
+        Vector3 pos = Vector3.zero;
+        float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+        float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = Utils.camBounds.max.y + enemySpawnPadding;
+        go.transform.position = pos;
+        //call SpawnEnemy() again in a couple of seconds
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
 	// Use this for initialization
 	void Start () {
 	
